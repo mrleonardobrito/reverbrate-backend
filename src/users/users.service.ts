a061cf9f9cde3a2d320c6fe19102fc35bdd3b859
mrleonardobrito <lbritogit@gmail.com>
2025-07-18 14:27:03 -0300

    adding search user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,8 @@ import { UserRepository } from './interfaces/user-repository.interface';
 import { UserResponseDto } from './dtos/user-response.dto';
 import { ReviewRepository } from 'src/reviews/interfaces/review-repository.interface';
 import { ListRepository } from 'src/lists/interfaces/list-repository.interface';
+import { PaginatedResponse } from 'src/common/http/dtos/paginated-response.dto';
+import { SearchUsersDto } from './dtos/search-users.dto';
 
 @Injectable()
 export class UsersService {
@@ -35,5 +37,37 @@ export class UsersService {
     );
 
     return new UserResponseDto(user, reviews, lists);
+  }
+
+  async searchUser(searchDto: SearchUsersDto): Promise<PaginatedResponse<UserResponseDto>> {
+    const { query, limit = 20, offset = 0 } = searchDto;
+
+    const users = await this.userRepository.searchUser(query, { limit, offset });
+
+    if (users.total === 0) {
+      throw new HttpException('No users found', HttpStatus.NOT_FOUND);
+    }
+
+    return {
+      data: await Promise.all(users.data.map(async user => {
+        const reviews = await this.reviewRepository.findAll(user.id, {
+          limit: 1000000,
+          offset: 0,
+        });
+        const lists = await this.listRepository.findAll(
+          {
+            limit: 1000000,
+            offset: 0,
+          },
+          user.id,
+        );
+        return new UserResponseDto(user, reviews, lists);
+      })),
+      total: users.total,
+      limit: users.limit,
+      offset: users.offset,
+      next: users.next,
+      previous: users.previous,
+    };
   }
 }
