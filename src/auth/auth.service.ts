9d5c64659bed2ff9edbd6de5dd186ac89ffe1538
mrleonardobrito <lbritogit@gmail.com>
2025-07-25 17:41:53 -0300

    adding cookies log

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -49,6 +49,7 @@ export class AuthService {
   }
 
   extractToken(req: Request) {
+    console.log(req.cookies);
     const accessToken = req.cookies?.['access_token'] as string | undefined;
     const refreshToken = req.cookies?.['refresh_token'] as string | undefined;
     const expiresIn = req.cookies?.['expires_in'] as number | undefined;
